7864fcdc4eaa084e7bc04f3af9a62c11c5040012
capstongroup3 <capstongroup3@gmail.com>
2024-08-13 02:07:21 -0400

    Update db.ts

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,13 +2,16 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://palavdesai5142:Palav@1234@cluster0.m5hymqu.mongodb.net/MediCure');
+        await mongoose.connect('mongodb+srv://palavdesai5142:Palav@1234@cluster0.m5hymqu.mongodb.net/MediCure', {
+            tls: true,
+            tlsAllowInvalidCertificates: false, // ensure you're using valid certificates
+        });
         console.log('MongoDB connected...');
     } catch (err) {
         if (err instanceof Error) {
-            console.error(err.message);
+            console.error('MongoDB connection error:', err.message);
         } else {
-            console.error('Unexpected error', err);
+            console.error('Unexpected error:', err);
         }
         process.exit(1);
     }
